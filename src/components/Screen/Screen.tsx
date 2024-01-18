b2fa46e295e9f6eeee1e4ce14b2e25f7677e5353
Celso Avelino <celsoavelino@MacBook-Pro-de-Celso.local>
2024-01-18 00:33:29 -0300

    PostItem

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -3,12 +3,12 @@ import {KeyboardAvoidingView, Platform} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 
-import {Icon, Text, Box, TouchableOpacityBox} from '@components';
+import {Icon, Text, Box, TouchableOpacityBox, BoxProps} from '@components';
 import {useAppTheme, useAppSafeArea} from '@hooks';
 
 import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer';
 
-interface ScreenProps {
+interface ScreenProps extends BoxProps {
   children: React.ReactNode;
   canGoBack?: boolean;
   scrollable?: boolean;
@@ -18,6 +18,8 @@ export function Screen({
   children,
   canGoBack = false,
   scrollable = false,
+  style,
+  ...boxProps
 }: ScreenProps) {
   const {top, bottom} = useAppSafeArea();
   const {colors} = useAppTheme();
@@ -34,7 +36,8 @@ export function Screen({
         <Box
           paddingBottom="s24"
           paddingHorizontal="s24"
-          style={{paddingTop: top, paddingBottom: bottom}}>
+          style={[{paddingTop: top, paddingBottom: bottom}, style]}
+          {...boxProps}>
           {canGoBack && (
             <TouchableOpacityBox
               onPress={navigation.goBack}
